ba40a776cbd28e5f45abed9aba9168fac528f297
Chinedu Okorie <okoriechinedusunday@gmail.com>
2024-03-06 09:45:25 +0100

    implement register controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,24 @@
 const { User } = require("../models/User");
+const { hashUserPassword } = require("../utils/passwordManipulation");
 exports.register = async (req, res) => {
   try {
-  } catch (error) {}
+    const { password } = req.body;
+    let hashedPassword = await hashUserPassword(password);
+    req.body.password = hashedPassword;
+    const createdUser = await User.create(req.body);
+    res.status(201).json({
+      statusCode: 201,
+      responseText: "SUCCESS",
+      data: { resource: createdUser, msg: "User created successfully" },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      statusCode: 500,
+      responseText: "FAIL",
+      data: { resource: {}, msg: "Could not create user something went wrong" },
+    });
+  }
 };
 exports.login = async (req, res) => {
   try {
